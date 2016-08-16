6f7d54a0753b980ba9b442bb6c8191663f70f1f3
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-08-16 19:29:56 +1200

    babel loader in config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  entry: ["./app.js", "./global.js"],
-  output: {
-    filename: "bundle.js"
-  },
-  watch: true
+ entry: ["./global.js" , "./app.js"],
+ output: {
+   filename: "bundle.js"
+ },
+ module: {
+   loaders: [
+     {
+       test: /\.es6$/,
+       exclude: /node_modules/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['react', 'es2015']
+       }
+     }
+   ]
+ },
+ resolve: {
+   extensions: ['', '.js', '.es6']
+ },
 }
